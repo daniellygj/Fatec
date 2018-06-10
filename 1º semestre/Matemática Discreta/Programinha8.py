ac5068768b3011c4d55d183abb63f13d85038e60
daniellygj <daniellygj@gmail.com>
2018-06-10 00:16:08 -0300

    programinha8 closes #40

--- 1º semestre/Matemática Discreta/Programinha8.py
+++ 1º semestre/Matemática Discreta/Programinha8.py
@@ -3,6 +3,7 @@ from console import readsite
 
 imprimir(readsite('http://dontpad.com/TheDoctor/md/fechoreflexivo.txt'))
 
+
 def namatriz(matriz):
     print('\nNa matriz:')
     for c in matriz:
@@ -14,11 +15,11 @@ def validacao(qtd):
         qtd = int(qtd)
         while qtd > 10:
             print('Digite um número menor que 10')
-            qtd = input('Quantas relações deseja adicionar? (MAX 10)')
+            qtd = input('2Quantas relações deseja adicionar? (MAX 10)')
             validacao(qtd)
     except ValueError:
         print('Informação inválida, tente novamente.')
-        qtd = qtd = input('Quantas relações deseja adicionar? (MAX 10)')
+        qtd = input('3Quantas relações deseja adicionar? (MAX 10)')
         validacao(qtd)
     return int(qtd)
 
@@ -26,57 +27,47 @@ def validacao(qtd):
 opc = 's'
 
 print("Fecho Reflexivo")
+
 while opc == 's':
-    while opc == 's':
-        qtd = input('Quantas relações deseja adicionar?')
-        qtd = validacao(qtd)
-        print("Entre com a relação (Utilize apenas 1 2 e 3)")
+    qtd = input('1Quantas relações deseja adicionar?')
+    qtd = validacao(qtd)
+    print("1Entre com a relação (Utilize apenas 1 2 e 3)")
 
-        relacoes = []
+    relacoes = []
 
-        for c in range(0, qtd):
+    for c in range(0, qtd):
+        temp = input(f'{c+1}º ')
+        while temp in relacoes:
+            print('Relação já adicionada. Entre com outra')
             temp = input(f'{c+1}º ')
-            while temp in relacoes:
-                print('Relação já adicionada. Entre com outra')
-                temp = input(f'{c+1}º ')
-                while temp not in '123':
-                    print('Utilize apenas os números 1, 2, 3 e 4. Digite novamente.')
-                    temp = (input(f'{c+1}º '))
-                if temp not in relacoes:
-                    break
-            relacoes.append(temp)
-
-
-
+            while temp not in '123':
+                print('Utilize apenas os números 1, 2, 3 e 4. Digite novamente.')
+                temp = (input(f'{c+1}º '))
+            if temp not in relacoes:
+                break
+        relacoes.append(temp)
 
     matriz = [[0, 0, 0],
               [0, 0, 0],
               [0, 0, 0]]
 
-
     for c2 in relacoes:
-        matriz[int(c2[:1])-1][int(c2[2:])-1] = 1
         matriz[int(c2[:1]) - 1][int(c2[2:]) - 1] = 1
-
-
 
     if matriz[0][0] == 1 and matriz[1][1] == 1 and matriz[2][2] == 1:
         print("A relação que você digitou já é reflexiva, então o fecho é a própria relação")
 
     else:
-        print('A relação que você digitou, não é reflexiva. O fecho reflexivo é: ')
-        if matriz[0][0] != 1:
-            matriz[0][0] = 1
-            print("O termo (1;1) foi adicionado a relação.")
-            relacoes.append('1;1')
-        if matriz[1][1] != 1:
-            matriz[1][1] = 1
-            print("O termo (2;2) foi adicionado a relação.")
-            relacoes.append('2;2')
-        if matriz[2][2] != 1:
-            matriz[2][2] = 1
-            print("O termo (3;3) foi adidionado a relação.")
-            relacoes.append('3;3')
+        print('A relação que você digitou, não é reflexivo.')
+        for c in range(0, 3):
+            if matriz[c][c] != 1:
+                matriz[c][c] = 1
+                temp = f'{c+1};{c+1}'
+            if temp not in relacoes:
+                print(f"A relação ({c+1};{c+1}) foi adicionada.")
+                relacoes.append(temp)
+            else:
+                print(f'A relação ({c};{c}) seria adicionada, mas ela já existe.')
         print(f'O fecho reflexivo da endorrelação é: {relacoes}')
         namatriz(matriz)
 
@@ -86,4 +77,4 @@ while opc == 's':
             print("Opção inválida, tente novamente.")
             opc = input('Deseja adicionar outra relação? ').lower
 
-#By Danny
+# By Danny
