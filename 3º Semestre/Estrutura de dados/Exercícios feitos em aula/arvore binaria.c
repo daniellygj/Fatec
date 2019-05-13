6f44b172b93c4782aaca8d3177a7c22bab36a057
Danielly <daniellygj@gmail.com>
2019-05-13 14:27:28 -0300

    Inserindo na arvore binaria

---
+++ 3º Semestre/Estrutura de dados/Exercícios feitos em aula/arvore binaria.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ARVORE {
+    int valor;
+    struct ARVORE *dir;
+    struct ARVORE *esq;
+};
+
+typedef struct ARVORE arvore;
+// ESQ se for menor
+// DIR se for maior
+// E < R < D
+
+void imprimeArvore(arvore *raiz) {
+   if (raiz != NULL) {
+        imprimeArvore(raiz->esq);
+        printf("%d ", raiz->valor);
+        imprimeArvore(raiz->dir);
+   }
+}
+
+void* insereArvore(arvore **r, int valor) {
+
+    if (*r == NULL)  {
+        (*r) = malloc(sizeof(arvore));
+        (*r)->valor = valor;
+        (*r)->dir = NULL;
+        (*r)->esq = NULL;
+
+    } else {
+        if (valor < (*r)->valor) {
+             insereArvore(&(*r)->esq, valor);
+        } else {
+            insereArvore(&(*r)->dir, valor);
+        }
+    }
+}
+
+int main() {
+    arvore *sla = NULL;
+
+    //sla = insereArvore(sla, 10);
+    insereArvore(&sla, 5);
+    insereArvore(&sla, 1);
+    insereArvore(&sla, 2);
+    insereArvore(&sla, 6);
+    insereArvore(&sla, 3);
+    insereArvore(&sla, 7);
+    insereArvore(&sla, 4);
+    insereArvore(&sla, 10);
+    insereArvore(&sla, 9);
+    insereArvore(&sla, 8);
+
+
+    imprimeArvore(sla);
+    printf("\n");
+}
